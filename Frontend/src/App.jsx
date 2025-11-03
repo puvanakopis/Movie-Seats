baf68c9a5605fc3d1f15c8f3023cb2490f0b4122
puvanakopis <puvanakopis@gmail.com>
2025-11-03 22:30:53 +0530

    Add Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast'
+
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetails from './pages/MovieDetails';
+import SeatLayout from './pages/SeatLayout';
+import MyBookings from './pages/MyBookings';
+import Favorites from './pages/Favorites';
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
 
-export default App
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-booking' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorites />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+};
+
+export default App;
